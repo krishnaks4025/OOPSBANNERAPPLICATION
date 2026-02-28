e23ebe6670a541964a8d44c7a9f402ae7c8dfb18
krishnaks4025 <srivastavakrishna2006@gmail.com>
2026-02-28 15:19:34 +0530

    Add files via upload

---
+++ oopsbanneruc5.java
@@ -0,0 +1,22 @@
+class oopsbanneruc5 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+			String.join(" ","   ***        ***      ******        ***** "),
+            String.join(" "," **   **    **   **    **    **     **     "),
+            String.join(" ","**     **  **     **   **     **   **     "),
+            String.join(" ","**     **  **     **   **    **     **  "),
+            String.join(" ","**     **  **     **   ******        ***"),
+            String.join(" ","**     **  **     **   **              **  "),
+            String.join(" ","**     **  **     **   **               **     "),
+            String.join(" "," **   **    **    **   **              **       "),
+            String.join(" ","   ***        ***      **         *****   ") 
+        };
+
+        
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
